fed1b92359b480be7f9c760d350c506418988c9e
Taekyeom Lee <96657494+taekyeomlee@users.noreply.github.com>
2024-01-23 00:24:59 +0900

    chore: Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'taekyeom',
   email: 'tkyeom96@gmail.com',
   link: 'https://blog.taekyeomlee.com',
-  description: 'This gonna be an awesome website.',
+  description: 'I am a front-end developer.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Seoul', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
